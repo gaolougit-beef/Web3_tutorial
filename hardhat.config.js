f9c670e85dc93b07773019f5d66e8607dbe2c075
张丽 <zhangli@zhanglideMacBook-Pro.local>
2025-03-31 18:39:08 +0800

    codes for lesson 5

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,17 +3,24 @@ require("@chainlink/env-enc").config();
 // require("./tasks/deploy-fundme");
 // require("./tasks/interact-fundme");
 require("./tasks");
-
+require("hardhat-deploy");
+require("@nomicfoundation/hardhat-ethers");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
 
 const SEPOLIA_URL = process.env.SEPOLIA_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const PRIVATE_KEY_1 = process.env.PRIVATE_KEY_1;
-
+ 
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  defaultNetwork: "hardhat",
+  mocha: {
+    timeout: 300000
+  },
   networks: {
     hardhat: {
     },
@@ -30,4 +37,12 @@ module.exports = {
       sepolia: ETHERSCAN_API_KEY
     }
   },
+  namedAccounts:{
+    firstAccount:{
+      default: 0
+    },
+    secondAccount:{
+      default: 1
+    }
+  }
 };
